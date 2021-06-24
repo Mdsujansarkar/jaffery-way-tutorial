848784fc9e39ece0a353f3df719c8460b3dc8c19
Mdsujansarkar <sujansarkar786@gmail.com>
2021-06-24 18:12:23 +0600

    init commit

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+      <title>Registration</title>
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-6 mx-auto" style="margin-top: 50px;">
+               <div class="card" style="background-color: #ecf0f1;">
+                  <div class="card-title mb-2">
+                     <h2 class="text-center mt-3">Registration Heading</h2>
+                  </div>
+                  <div class="card-body">
+                     <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">User Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="User Name">
+                     </div>
+                     <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name">
+                     </div>
+                     <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                     </div>
+                     <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
+                     </div>
+                     <button type="button" class="btn btn-primary">Primary</button>
+                     
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+   </body>
+</html>
